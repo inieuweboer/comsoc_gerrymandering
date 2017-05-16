a3c3ae09baae03b7f88ae443c77fee9cf0790401
Ismani Nieuweboer <inieuweboer@users.noreply.github.com>
2017-05-16 12:45:14 +0200

    divide into equal districts

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from math import sqrt
+from itertools import product
+
 class Voter:
     def __init__(self):
         self.preferences = {1: 'a', 2:'b', 3:'c'}
@@ -11,7 +14,7 @@ class Grid:
         self.size = size
         self.grid = {}
         for x in range(size):
-            self.grid[x] = {} 
+            self.grid[x] = {}
             for y in range(size):
                 self.grid[x][y] = Voter()
 
@@ -20,7 +23,32 @@ class Grid:
         for x in range(self.size):
             for y in range(self.size):
                 profile.append(self.grid[x][y])
+
         return profile
+
+    def divide_equal_districts(self, no_distrs):
+        # Create a list of districts. The square that is nearest low to no_districts is used
+        # We also assume the square root divides self.size
+        sqrt_distrs = int(sqrt(no_distrs))
+        div = self.size / sqrt_distrs
+
+        self.districts = []
+
+        # in x range
+        for i in xrange(sqrt_distrs):
+            for j in xrange(sqrt_distrs):
+                # Initially the cartesian product of equally sized lists
+                grid_points = list(product(xrange(div*i, div*(i+1)), xrange(div*j, div*(j+1))))
+
+                self.districts.append(District(grid_points))
+
+class District:
+    def __init__(self, grid_points):
+        # This is a list of grid points that belong to this district
+        self.grid_points = grid_points
+
+    def __str__(self):
+        return str(self.grid_points)
 
 
 def rule_plurality(profile):
@@ -44,10 +72,14 @@ def rule_borda(profile):
 
 
 def main():
-    grid = Grid(5)
+    grid = Grid(12)
     score_plur = rule_plurality(grid.profile())
 
     print(score_plur)
+
+    grid.divide_equal_districts(4)
+    for distr in grid.districts:
+        print(distr)
 
 
 if __name__ == "__main__":
